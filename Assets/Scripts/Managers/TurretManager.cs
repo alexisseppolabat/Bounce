6530cf3af37cd1e8e00f13ac35debb79d0b1ba92
Alexis Labat <alexislabat@hotmail.com>
2021-06-30 15:53:16 +0100

    Modified the turret script so that the turret only looks at the player in the hemisphere above its horizon. Also made the laser have firing abilities, however at this current stage it is buggy and doesn't fire on the player.

--- Assets/Scripts/Managers/TurretManager.cs
+++ Assets/Scripts/Managers/TurretManager.cs
@@ -1,15 +1,72 @@
-﻿using System;
+﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace Managers {
     public class TurretManager : MonoBehaviour {
         public Transform ballTransform;
+        public Material laser;
+        public float lag = 0.2f;
+        public float chargeTime = 1.25f;
+
+        private bool isAbove;
+
+        public Transform aim;
+        public Transform fire;
         
         private void FixedUpdate() {
+            StartCoroutine(nameof(UpdatePosition));
+        }
+
+
+        private IEnumerator UpdatePosition() {
+            Vector3 position = transform.position;
             Vector3 target = ballTransform.position;
-            target.y = Mathf.Max(target.y, transform.position.y);
-            transform.LookAt(target);
+            Vector3 offset = target - position;
+
+            yield return new WaitForSeconds(lag);
+            
+            if (target.y >= transform.position.y) {
+                transform.LookAt(target);
+                
+                if (Physics.Raycast(position, offset, out RaycastHit hit, Mathf.Infinity)) {
+                    DrawLine(position, hit.point, Color.red);
+                    aim.position = hit.point;
+                    isAbove = true;
+                    yield return new WaitForSeconds(chargeTime);
+                    
+
+                    // Update position again and ensure the ball hasn't dropped too far
+                    target = ballTransform.position;
+                    if (target.y < transform.position.y) yield break;
+                    
+                    yield return new WaitForSeconds(lag);
+                    if (target.y >= transform.position.y && Physics.Raycast(position, offset, out hit, Mathf.Infinity)) {
+                        DrawLine(position, hit.point, Color.red, 0.2f);
+                        fire.position = hit.point;
+                    }
+                }
+            } else {
+                isAbove = false;
+                target.y = transform.position.y;
+                transform.LookAt(target);
+            }
+        }
+        
+        private void DrawLine(Vector3 start, Vector3 end, Color color, float thickness = 0.01f) {
+            GameObject myLine = new GameObject();
+            
+            myLine.transform.position = start;
+            myLine.AddComponent<LineRenderer>();
+            
+            LineRenderer renderer = myLine.GetComponent<LineRenderer>();
+            
+            renderer.material = laser;
+            renderer.SetColors(color, color);
+            renderer.SetWidth(thickness, thickness);
+            renderer.SetPosition(0, start);
+            renderer.SetPosition(1, end);
+            
+            Destroy(myLine, 0.01f);
         }
     }
 }
